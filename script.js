3a62a09efcc70906ba7894a7b5b57135341c6501
ashishbhartii <109731651+ashishbhartii@users.noreply.github.com>
2024-05-11 21:09:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// your code here
+function appendFunc() {
+    const name = document.getElementById("name").value;
+    const year = document.getElementById("year").value;
+    const myH3 = document.getElementById("myH3");
+
+    if (name === '' && year === '') {
+        myH3.textContent = "https://localhost:8080/";
+    } else if (name !== '' && year !== '') {
+        myH3.textContent = `https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+    } else if (name !== '') {
+        myH3.textContent = `https://localhost:8080/?name=${encodeURIComponent(name)}`;
+    } else if (year !== '') {
+        myH3.textContent = `https://localhost:8080/?year=${encodeURIComponent(year)}`;
+    }
+
+    // Prevent form submission to another page
+    return false;
+}
